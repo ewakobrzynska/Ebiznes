889ee93a497d2caebad586103cab3b24bd12327f
Ewa <ewakobrzynska1709@gmail.com>
2024-06-10 22:19:36 +0200

    bf

--- zadanie10/backend/src/test/java/com/example/demo/BackendApplicationTests.java
+++ zadanie10/backend/src/test/java/com/example/demo/BackendApplicationTests.java
@@ -3,11 +3,12 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BackendApplicationTests {
 
-	@Test
+	@@Test
 	void contextLoads() {
+		System.out.println("Test contextLoads() is running...");
 	}
-
+	
 }
